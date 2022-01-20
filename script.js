7be15c7fd68283f458238279eb1ade88eb9f1dbc
hakareem <harithakareem72.com>
2022-01-20 19:18:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const daysEL = document.getElementById("days");
+const hoursEL = document.getElementById("hours");
+const minsEL = document.getElementById("mins");
+const secondsEL = document.getElementById("seconds");
+
+const birthday = "4 Jun 2022";
+
+function countdown() {
+  const birthdayDate = new Date(birthday);
+  const currentDate = new Date();
+
+  const totalSeconds = (birthdayDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+
+  const hours = Math.floor((totalSeconds / 3600) % 24);
+
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEL.innerHTML = formatTime(days);
+  hoursEL.innerHTML = formatTime(hours);
+  minsEL.innerHTML = formatTime(mins);
+  secondsEL.innerHTML = formatTime(seconds);
+
+  console.log(days, hours, mins);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown, 1000);
+
+countdown();
